81b81f6ce6ad6ee526fc257e231bb3abb09eae6f
nivo0616 <nikita.vorobev@netcracker.com>
2025-04-20 21:06:48 +0300

    test

--- apps/cms/config/server.ts
+++ apps/cms/config/server.ts
@@ -1,7 +1,12 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  host: '0.0.0.0', // Принудительно задаем хост
+  url: env('PUBLIC_URL', 'https://your-app.vercel.app'), // Указываем полный URL
+  admin: {
+    autoOpen: false // Отключаем автооткрытие админки
+  }
+  // host: env('HOST', '0.0.0.0'),
+  // port: env.int('PORT', 1337),
+  // app: {
+  //   keys: env.array('APP_KEYS'),
+  // },
 });
